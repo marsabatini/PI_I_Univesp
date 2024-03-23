84c4814967d01631f4a4a039cec9a2b0dd325bfc
marsabatini <marco_rsabatini@hotmail.com>
2024-03-23 19:37:47 -0300

    Criação da Entidade Aluno

---
+++ 02_Back-end/Cod_Java/src/main/java/model/Aluno.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Aluno {
+
+    String name;
+
+    int idade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
