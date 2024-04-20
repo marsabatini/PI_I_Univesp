3203a416e5e3b784685a728b6aa2c56a4216b681
Leonardo Carmo <leocarmoja12@outlook.com>
2024-04-20 13:08:39 -0300

    Insert dos Planos

---
+++ 04_MySQL/Plano_Insert.sql
@@ -0,0 +1,117 @@
+INSERT INTO planos
+(
+    nome,
+    qtd_aulas,
+    taxa_matricula,
+    kimono,
+    ferias,
+    tranferivel
+)
+VALUES
+(
+    'WHITE BELT',
+    1,
+    99,
+    479,
+    0,
+    0
+)
+
+VALUES
+(
+    'BLUE BELT',
+    2,
+    99,
+    479,
+    0,
+    0
+)
+
+VALUES
+(
+    'PURPLE BELT',
+    0,
+    99,
+    479,
+    0,
+    0
+)
+
+VALUES
+(
+    'KIDS',
+    1,
+    99,
+    479,
+    0,
+    0
+)
+
+INSERT INTO planos
+(
+    nome,
+    qtd_aulas,
+    ferias,
+    convidado,
+    tranferivel
+)
+
+VALUES
+(
+    'BLACK BELT',
+    0,
+    1,
+    1,
+    1
+)
+
+VALUES
+(
+    'SUPER KIDS',
+    0,
+    1,
+    1,
+    1
+)
+
+VALUES
+(
+    'SINGLE +',
+    2,
+    1,
+    1,
+    1
+)
+
+VALUES
+(
+    'TRIPLE +',
+    2,
+    1,
+    1,
+    1
+)
+
+INSERT INTO planos
+(
+    nome,
+    qtd_aulas,
+    taxa_matricula
+)
+
+VALUES 
+(
+    'SINGLE',
+    2,
+    99
+)
+
+VALUES 
+(
+    'TRIPLE',
+    2,
+    99
+)
+
+
+
