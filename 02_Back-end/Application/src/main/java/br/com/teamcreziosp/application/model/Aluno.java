ee6bc915a2827a53a25f0f519285eb947dd66ca3
Amanda Zaine <amandazaine@outlook.com>
2024-04-27 00:57:57 -0300

    criando models

--- 02_Back-end/Application/src/main/java/br/com/teamcreziosp/application/model/Aluno.java
+++ 02_Back-end/Application/src/main/java/br/com/teamcreziosp/application/model/Aluno.java
@@ -1,4 +1,39 @@
 package br.com.teamcreziosp.application.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "aluno")
+@Getter
+@Setter
 public class Aluno {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String sexo;
+    private String email;
+    private String rg;
+    private String cpf;
+    private String telefone;
+    private String graduacao;
+    private String endereco;
+    private String exameMedico;
+    private Integer qtddAulasProxGrad;
+    private Integer plano;
+    private String statusPlano;
+    private String usuario;
+    private String senha;
+    private String tipoUsuario;
 }
--- 02_Back-end/Application/src/main/java/br/com/teamcreziosp/application/model/Aula.java
+++ 02_Back-end/Application/src/main/java/br/com/teamcreziosp/application/model/Aula.java
@@ -1,4 +1,32 @@
 package br.com.teamcreziosp.application.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "aluno")
+@Getter
+@Setter
 public class Aula {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer professor;
+    private LocalDate data;
+    private LocalTime hora;
+    private String sexoIndicado;
+    private Integer plano;
+    private String modeloAula;
+    private Integer limiteParticipantes;
+    private Integer idadeMin;
+    private Integer idadeMax;
+
 }
