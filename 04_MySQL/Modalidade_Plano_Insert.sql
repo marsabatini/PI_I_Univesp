133a02ba64069418d89518a6953432bb8e80cfa4
Leonardo Carmo <leocarmoja12@outlook.com>
2024-04-27 16:53:49 -0300

    Insert modalidades de pagamento

---
+++ 04_MySQL/Modalidade_Plano_Insert.sql
@@ -0,0 +1,107 @@
+-- ------------------------------------------------
+-- Inserção plano 'White Belt' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(1,399,1,1),
+(1,523,4,6),
+(1,349,6,6),
+(1,598,6,12),
+(1,299,12,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Blue Belt' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(2,449,1,1),
+(2,598.50,4,6),
+(2,399,6,6),
+(2,698,6,12),
+(2,349,12,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Purple Belt' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(3,449,1,1),
+(3,673.50,4,6),
+(3,449,6,6),
+(3,798,6,12),
+(3,399,12,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Kids' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(4,689,1,1),
+(4,778.50,4,6),
+(4,519,6,6),
+(4,718,6,12),
+(4,359,12,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Black Belt' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(5,299,12,12),
+(5,358.80,10,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Super Kids' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(6,349,12,12),
+(6,418.80,10,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Single +' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(7,1199,12,12),
+(7,1438.80,10,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Triple +' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(8,399,12,12),
+(8,478.80,10,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Single' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(9,1999,1,1),
+(9,2428.50,4,6),
+(9,1619,6,6),
+(9,2498,6,12),
+(9,1249,12,12);
+
+-- ------------------------------------------------
+-- Inserção plano 'Triple' formas de pagamento 
+-- ------------------------------------------------
+INSERT INTO teamcreziosp.modalidade_plano
+(plano,valor_parcela,numero_parcela, validade)
+VALUES
+(10,698,1,1),
+(10,838.50,4,6),
+(10,559,6,6),
+(10,858,6,12),
+(10,429,12,12);
