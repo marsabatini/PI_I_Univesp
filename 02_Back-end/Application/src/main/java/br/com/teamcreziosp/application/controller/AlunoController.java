bd617cda7f2790625f0a2c5cef53be69f9b9349a
Amanda Zaine <amandazaine@outlook.com>
2024-05-21 23:39:25 -0300

    Funcionalidade agendamento

--- 02_Back-end/Application/src/main/java/br/com/teamcreziosp/application/controller/AlunoController.java
+++ 02_Back-end/Application/src/main/java/br/com/teamcreziosp/application/controller/AlunoController.java
@@ -1,11 +1,43 @@
 package br.com.teamcreziosp.application.controller;
 
+import br.com.teamcreziosp.application.model.Aula;
+import br.com.teamcreziosp.application.repository.AulaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/adm")
 public class AlunoController {
 
+    @Autowired
+    private AulaRepository aulaRepository;
 
+    @GetMapping("/aulas")
+    public List<Aula> findAll() {
+        return aulaRepository.findAll();
+    }
 
+    @PostMapping("/aulas")
+    public void save(@RequestBody Aula aula) {
+        aulaRepository.save(aula);
+    }
+
+    @GetMapping("/aulas/{id}")
+    public Optional<Aula> findById(@PathVariable(value = "id") Integer id) {
+        return aulaRepository.findById(id);
+    }
+
+    @DeleteMapping("/aulas")
+    public void delete(@RequestBody Aula aula) {
+        aulaRepository.delete(aula);
+    }
+
+    @PutMapping("/aulas")
+    public Aula update(@RequestBody Aula aula) {
+        return aulaRepository.save(aula);
+    }
 }
